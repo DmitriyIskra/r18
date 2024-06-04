8f287f9faec91d38fdfba450a89db87752f6001c
Dmitriy Iskra <dmitriyiskra@mail.ru>
2024-06-04 11:31:57 +0300

    created page price mobile ver.

--- webpack.config.js
+++ webpack.config.js
@@ -188,6 +188,7 @@ module.exports = {
             { from: 'src/img/content/accessories-thermometer-motta-content.webp', to: 'img/content' },
 
             { from: 'src/files/conditions-ctm.pdf', to: 'files/conditions-ctm.pdf' },
+            { from: 'src/files/price.pdf', to: 'files/price.pdf' },
 
             { from: 'src/img/favicon/apple-touch-icon.png', to: 'img/favicon' },
             { from: 'src/img/favicon/32.png', to: 'img/favicon' },
